228e39572f0d1e713508bb62ba05bf70a7e45fe6
Yann <227698+yanncarlier@users.noreply.github.com>
2025-05-19 17:48:02 +0700

    createWindow size and  electron pkg versions

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const { app, BrowserWindow } = require('electron');
 
 function createWindow() {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1080,
+        height: 1060,
     });
     win.loadFile('index.html');
 }
